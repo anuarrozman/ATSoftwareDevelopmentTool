b99f3e45ea482fbcefd3f138ca10235b9f06a014
anuarrozman <anuar.rozman@airdroitech.com>
2024-03-26 13:40:54 +0800

    Fix script

--- Sprint14-Prototype/build.sh
+++ Sprint14-Prototype/build.sh
@@ -12,7 +12,7 @@ pip install hidapi --break-system-packages
 # Install hid
 pip install hid --break-system-packages
 
-export ProdTool_Path = ${HOME}/ATSoftwareDevelopmentTool/Sprint14-Prototype/
+export ProdTool_Path=${HOME}/ATSoftwareDevelopmentTool/Sprint14-Prototype/
 
 # Build the project
 pyinstaller --onefile main.py --hidden-import esptool --hidden-import pyserial --hidden-import hidapi --distpath ${ProdTool_Path}
