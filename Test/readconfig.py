74ace7e0200039cc080b0ed9ccf289c2ae3e44f1
anuarrozman <anuar.rozman@airdroitech.com>
2024-05-15 13:40:01 +0800

    Fix esptool command

---
+++ Test/readconfig.py
@@ -0,0 +1,19 @@
+import configparser
+
+# Create a ConfigParser object
+config = configparser.ConfigParser()
+
+# Read the config.ini file
+config.read('/home/anuar/ProdTools/ATSoftwareDevelopmentTool/Test/config.ini')
+
+# Accessing values from sections
+value1 = config['Section1']['key1']
+value2 = config['Section1']['key2']
+value3 = config['Section2']['key3']
+value4 = config['Section2']['key4']
+
+# Print the values
+print("Value 1:", value1)
+print("Value 2:", value2)
+print("Value 3:", value3)
+print("Value 4:", value4)
