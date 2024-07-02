1980eed5e1978dd3e8c36f80328825174087829f
anuarrozman <anuar.rozman@airdroitech.com>
2024-07-02 17:38:56 +0800

    new commit

--- FlashingTool/main.py
+++ FlashingTool/main.py
@@ -525,27 +525,27 @@ class SerialCommunicationApp:
         config = configparser.ConfigParser()
         config.read(ini_file_path)
 
-        if "flash" in config:
-            logger.info("Flashing firmware and certificate")
-            port = config.get("flash", "port")
-            baud = config.get("flash", "baud")
-            logger.info(f"Port: {port}, Baud: {baud}")
-            self.flash_firmware(port, baud)
-            self.flash_cert(port) 
+        # if "flash" in config:
+        #     logger.info("Flashing firmware and certificate")
+        #     port = config.get("flash", "port")
+        #     baud = config.get("flash", "baud")
+        #     logger.info(f"Port: {port}, Baud: {baud}")
+        #     self.flash_firmware(port, baud)
+        #     self.flash_cert(port) 
 
-        if "dmm" in config:
-            logger.info("Reading multimeter")
-            self.dmmReader.select_device(0)
+        # if "dmm" in config:
+        #     logger.info("Reading multimeter")
+        #     self.dmmReader.select_device(0)
 
-        if "factory" in config:
-            logger.info("Entering factory mode")
+        # if "factory" in config:
+        #     logger.info("Entering factory mode")
 
-            try:
-                port = config.get("factory", "port")
-                baud = config.get("factory", "baud")
-                self.serialCom.open_serial_port(port, baud)
-            except configparser.NoOptionError:
-                logger.error("Port not found in the INI file")
+        #     try:
+        #         port = config.get("factory", "port")
+        #         baud = config.get("factory", "baud")
+        #         self.serialCom.open_serial_port(port, baud)
+        #     except configparser.NoOptionError:
+        #         logger.error("Port not found in the INI file")
 
         # Signal that task 1 is complete
         self.task1_completed.set()
@@ -576,60 +576,60 @@ class SerialCommunicationApp:
         config = configparser.ConfigParser()
         config.read(ini_file_path)
 
-        if "mac_address" in config:
-            logger.info("Reading MAC Address")
-            self.get_device_mac()
-            time.sleep(5)
+        # if "mac_address" in config:
+        #     logger.info("Reading MAC Address")
+        #     self.get_device_mac()
+        #     time.sleep(5)
 
-        if "mac_address" in config:
-            logger.info("Reading MAC Address")
-            self.get_device_mac()
-            time.sleep(5)
+        # if "mac_address" in config:
+        #     logger.info("Reading MAC Address")
+        #     self.get_device_mac()
+        #     time.sleep(5)
 
-        if "serial_number" in config:
-            logger.info("Writing Serial Number")
-            self.send_serial_number()
-            time.sleep(5)
+        # if "serial_number" in config:
+        #     logger.info("Writing Serial Number")
+        #     self.send_serial_number()
+        #     time.sleep(5)
 
-        if "matter_qr" in config:
-            logger.info("Writing Matter QR")
-            self.send_mqtr()
-            time.sleep(5)
+        # if "matter_qr" in config:
+        #     logger.info("Writing Matter QR")
+        #     self.send_mqtr()
+        #     time.sleep(5)
 
-        if "atbeam_temp" in config:
-            logger.info("Reading ATBeam Temperature")
-            self.get_atbeam_temp()
-            time.sleep(5)
+        # if "atbeam_temp" in config:
+        #     logger.info("Reading ATBeam Temperature")
+        #     self.get_atbeam_temp()
+        #     time.sleep(5)
 
-        if "atbeam_humid" in config:
-            logger.info("Reading ATBeam Humidity")
-            self.get_atbeam_humid()
-            time.sleep(5)
+        # if "atbeam_humid" in config:
+        #     logger.info("Reading ATBeam Humidity")
+        #     self.get_atbeam_humid()
+        #     time.sleep(5)
 
-        if "rgb" in config:
-            logger.info("LED Test")
+        # if "rgb" in config:
+        #     logger.info("LED Test")
 
-            try: 
-                red = config.get("rgb", "red", fallback=None)
-                green = config.get("rgb", "green", fallback=None)
-                blue = config.get("rgb", "blue", fallback=None)
-                if red:
-                    self.send_command("FF:3;RGB-1\r\n")
-                    logger.info("Red LED turned on")
-                    time.sleep(3)
-                if green: 
-                    self.send_command("FF:3;RGB-2\r\n")
-                    logger.info("Green LED turned on")
-                    time.sleep(3)
-                if blue:
-                    self.send_command("FF:3;RGB-3\r\n")
-                    logger.info("Blue LED turned on")
-                    time.sleep(3)
-                    self.send_command("FF:3;reboot\r\n")
-                if not (red or green or blue):
-                    logger.error("LED colors not found in the INI file")
-            except configparser.NoSectionError:
-                logger.error("RGB section not found in the INI file")
+        #     try: 
+        #         red = config.get("rgb", "red", fallback=None)
+        #         green = config.get("rgb", "green", fallback=None)
+        #         blue = config.get("rgb", "blue", fallback=None)
+        #         if red:
+        #             self.send_command("FF:3;RGB-1\r\n")
+        #             logger.info("Red LED turned on")
+        #             time.sleep(3)
+        #         if green: 
+        #             self.send_command("FF:3;RGB-2\r\n")
+        #             logger.info("Green LED turned on")
+        #             time.sleep(3)
+        #         if blue:
+        #             self.send_command("FF:3;RGB-3\r\n")
+        #             logger.info("Blue LED turned on")
+        #             time.sleep(3)
+        #             self.send_command("FF:3;reboot\r\n")
+        #         if not (red or green or blue):
+        #             logger.error("LED colors not found in the INI file")
+        #     except configparser.NoSectionError:
+        #         logger.error("RGB section not found in the INI file")
         
         if "servo" in config:
             logger.info("Pressing Button")
@@ -643,15 +643,15 @@ class SerialCommunicationApp:
             except configparser.NoOptionError:
                 logger.error("Servo configuration not found in the INI file")
 
-        if "temp_compare" in config:
-            logger.info("Temperature Comparison")
-            self.read_temp_aht20()
-            time.sleep(5)
+        # if "temp_compare" in config:
+        #     logger.info("Temperature Comparison")
+        #     self.read_temp_aht20()
+        #     time.sleep(5)
         
-        if "humid_compare" in config:
-            logger.info("Humidity Comparison")
-            self.read_humid_aht20()
-            time.sleep(5)
+        # if "humid_compare" in config:
+        #     logger.info("Humidity Comparison")
+        #     self.read_humid_aht20()
+        #     time.sleep(5)
 
     def start_task2_thread(self):
         self.task2_thread = threading.Thread(target=self.start_test2)
