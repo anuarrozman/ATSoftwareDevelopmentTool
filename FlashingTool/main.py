74c4ba043e2a897a578b647d0bab0d59e2a02a7b
anuarrozman <anuar.rozman@airdroitech.com>
2024-07-02 17:48:21 +0800

    new commit

--- FlashingTool/main.py
+++ FlashingTool/main.py
@@ -639,17 +639,15 @@ class SerialCommunicationApp:
                 button_angle = config.get("servo", "button_angle")
                 pressing_duration = config.get("servo", "pressing_duration")
 
-                float_pressing_time = float(pressing_time)
                 float_pressing_duration = float(pressing_duration)
+                int_pressing_time = int(pressing_time)
+                int_button_angle = int(button_angle)
 
-                # Convert float_pressing_time to an integer
-                int_pressing_time = int(float_pressing_time)
-
-                logger.info(f"Pressing button {int_pressing_time} times, angle: {button_angle}, duration: {float_pressing_duration}")
+                logger.info(f"Pressing button {int_pressing_time} times, angle: {int_button_angle}, duration: {float_pressing_duration}")
 
                 for i in range(int_pressing_time):
                     logger.info(f"Pressing button {i+1} time")
-                    self.servo_controller.set_angle(button_angle)
+                    self.servo_controller.set_angle(int_button_angle)
                     time.sleep(float_pressing_duration)
                     self.servo_controller.set_angle(0)
                     time.sleep(0.5)
